d536d27bd05cfc9b0372928b15c12eb9b33f79a8
chuhe <3568001975@qq.com>
2024-03-05 20:41:49 +0800

    调用喂价方法

--- src/Bank.sol
+++ src/Bank.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.2;
 
 import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+// 预言机的包 喂价
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Bank is AutomationCompatibleInterface {
     mapping(address => uint) public depositeAccount;
@@ -9,27 +11,48 @@ contract Bank is AutomationCompatibleInterface {
     address public owner;
     uint constant DEPOSITE_LIMIT = 0.001 ether;
     bool public rept = false;
-    uint public upperLimit;
+    uint public upperLimit = 0.002 ether;
     uint public depositeTotal;
+    // 预言机
+    AggregatorV3Interface internal dataFeed;
     error withdrawFail();
-    event WithdrawSuccess(address user,uint amount);
+    event WithdrawSuccess(address user, uint amount);
+    event Price(
+        uint80 roundID,
+        int answer,
+        uint startedAt,
+        uint timeStamp,
+        uint80 answeredInRound
+    );
 
     constructor() {
         owner = msg.sender;
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
     }
 
     // 接收eth
     receive() external payable {}
 
-    modifier checkAmount(uint amount){
-        require(amount >=  DEPOSITE_LIMIT,"Deposits should be greater than 0.001ether");
+    modifier checkAmount(uint amount) {
+        require(
+            amount >= DEPOSITE_LIMIT,
+            "Deposits should be greater than 0.001ether"
+        );
         _;
     }
 
     /**
       存款合约
     **/
-    function deposite() public payable checkAmount(msg.value) virtual returns (bool) {
+    function deposite()
+        public
+        payable
+        virtual
+        checkAmount(msg.value)
+        returns (bool)
+    {
         require(msg.value > 0, "The deposit should be greater than zero");
         depositeAccount[msg.sender] += msg.value;
         depositeTotal += msg.value;
@@ -81,17 +104,23 @@ contract Bank is AutomationCompatibleInterface {
         return address(this).balance;
     }
 
-    function checkUpkeep(bytes calldata) external view returns (bool overLimit, bytes memory ) {
+    function checkUpkeep(
+        bytes calldata
+    ) external view returns (bool overLimit, bytes memory) {
         overLimit = depositeTotal > upperLimit;
     }
 
-    function performUpkeep(bytes calldata /* performData */) external  {
+    function performUpkeep(bytes calldata /* performData */) external {
         uint transferAmount = depositeTotal / 2;
-        (bool success,) = owner.call{value: transferAmount}("");
+        (bool success, ) = owner.call{value: transferAmount}("");
         if (!success) {
             revert withdrawFail();
         }
         depositeTotal -= transferAmount;
-        emit WithdrawSuccess(owner, transferAmount);
+        // 获取ETH对usdt价格
+        (uint80 roundID, int answer,uint startedAt, uint timeStamp, uint80 answeredInRound) =  
+        dataFeed.latestRoundData();
+        // emit WithdrawSuccess(owner, transferAmount);
+        emit Price(roundID,answer,startedAt,timeStamp,answeredInRound);
     }
 }
